1ecbbc98a4a8dbba17bc35a1bcde6d62188044d0
Jon Stephan <jfstepha@gmail.com>
2012-10-01 21:27:53 -0400

    Changed from flat to an object.

--- nodes/range_filter.py
+++ nodes/range_filter.py
@@ -12,53 +12,60 @@ from std_msgs.msg import Int16
 from std_msgs.msg import Float32
 from numpy import array
 
-weight = 0.1  # 1 = this value only 0 = prev value only
-cur_val = 0
+############################################################################## 
+############################################################################## 
+class RangeFilter():
+############################################################################## 
+############################################################################## 
 
-rolling_ave = 0.0
-rolling_std = 0.0
-rolling_meters = 0.0
+    #########################################################################
+    def __init__(self):
+    #########################################################################
+        cur_val = 0
 
-def inputCallback(msg):
-    rospy.loginfo("-D- range_filter inputCallback")
-    b = 266
-    m = -1.31
-    cur_val = msg.data
+        self.rolling_ave = 0.0
+        self.rolling_std = 0.0
+        self.rolling_meters = 0.0
+        rospy.init_node("range_filter")
+        rospy.loginfo("-I- range_filter started")
+        self.rolling_pts = rospy.get_param('~rolling_pts',4)
+        self.prev = [0] * self.rolling_pts
     
-    global rolling_ave
-    global rolling_std
-    global prev
-    global std_pub
+        rospy.Subscriber("range", Int16, self.inputCallback)
     
-    if cur_val < 900:
-        prev.append(cur_val)
-        del prev[0]
+        self.filtered_pub = rospy.Publisher("range_filtered", Float32)
+        self.std_pub = rospy.Publisher("range_std", Float32)
+        while not rospy.is_shutdown():
+            rospy.spin()
+    
+
+    #########################################################################
+    def inputCallback(self, msg):
+    #########################################################################
+        rospy.loginfo("-D- range_filter inputCallback")
+        b = 266
+        m = -1.31
+        cur_val = msg.data
+    
+        if cur_val < 900:
+            self.prev.append(cur_val)
+            del self.prev[0]
         
-        p = array(prev)
-        rolling_ave = p.mean()
-        rolling_std = p.std()
+            p = array(self.prev)
+            self.rolling_ave = p.mean()
+            self.rolling_std = p.std()
         
-        p = array(prev)
-        rolling_ave = p.mean()
-        rolling_std = p.std()
+            rolling_meters = b * self.rolling_ave ** m
         
-        rolling_meters = b * rolling_ave ** m
-        
-        filtered_pub.publish( rolling_meters )
-        std_pub.publish( rolling_std )
+            self.filtered_pub.publish( rolling_meters )
+            self.std_pub.publish( self.rolling_std )
         
     
+############################################################################## 
+############################################################################## 
 if __name__ == '__main__':
+############################################################################## 
+############################################################################## 
     """ main"""
-    rospy.init_node("range_filter")
-    rospy.loginfo("-I- range_filter started")
-    rolling_pts = rospy.get_param('~rolling_pts',4)
-    global prev
-    prev = [0] * rolling_pts
+    rangeFilter = RangeFilter()
     
-    rospy.Subscriber("range", Int16, inputCallback)
-    
-    filtered_pub = rospy.Publisher("range_filtered", Float32)
-    std_pub = rospy.Publisher("range_std", Float32)
-    
-    rospy.spin()
